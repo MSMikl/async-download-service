a3cadc129ece1f5ec3bee86f64de6b08b5f08c74
MSMikl <michalbl4@gmail.com>
2022-11-26 22:14:48 +0600

    project acomplished

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
+import asyncio
+import logging
+import os
+
 from aiohttp import web
-import aiofiles
+from environs import Env
 
 
-async def archive(request):
-    raise NotImplementedError
+env = Env()
+env.read_env()
+
+LOGGING = env.bool('LOGGING', True)
+PHOTO_PATH = env('PHOTO_PATH', 'test_photos')
+DELAY = env.int('DELAY', 0)
+
+logger = logging.getLogger('server')
 
 
 async def handle_index_page(request):
@@ -12,10 +22,44 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+async def send_archive(request):
+    hash = request.match_info['archive_hash']
+    path = os.path.join(PHOTO_PATH, hash)
+    if not os.path.isdir(path):
+        raise web.HTTPNotFound(text='Архива не существует, или он был удален')
+    args = ['-r', '-9', '-', '.']
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'multipart/form-data'
+    response.headers['Content-Disposition'] = "attachment; filename=photos.zip"
+    await response.prepare(request)
+    process = await asyncio.create_subprocess_exec('zip', *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, cwd=path)
+    number = 0
+    try:
+        while not process.stdout.at_eof():
+            part = await process.stdout.read(500000)
+            if DELAY:
+                await asyncio.sleep(DELAY)
+            number += 1
+            logger.info(f"Sending archive chunk {number}")
+            await response.write(part)
+    except ConnectionResetError:
+        logger.warning('Downloading cancelled')
+        await process.communicate()
+        return
+    await process.communicate()
+    if process.returncode == 0: 
+        return response
+
+
 if __name__ == '__main__':
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
+    if LOGGING:
+        logger.addHandler(stream_handler)
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        web.get('/archive/{archive_hash}/', send_archive),
     ])
     web.run_app(app)
